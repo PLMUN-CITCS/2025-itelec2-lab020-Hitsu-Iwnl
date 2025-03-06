c100a3b46b52c2cbd04351b1fbfd36fb552fa540
Hitsu-Iwnl <foxsin902@gmail.com>
2025-03-06 19:38:01 +0800

    Submitting Python Week 05 - Laboratory # 20

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -1,18 +1,6 @@
-def get_integer_input():
-    """Prompt user for an integer input and return it."""
-    while True:
-        try:
-            return int(input("Enter an integer: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
+number = int(input("Enter an integer: "))
 
-def check_even_odd(number):
-    """Return a message indicating if the number is even or odd."""
-    if number % 2 == 0:
-        return f"{number} is an Even number."
-    else:
-        return f"{number} is an Odd number."
-
-# Main program
-num = get_integer_input()
-print(check_even_odd(num))
+if number % 2 == 0:
+    print(f"{number} is an Even number.")
+else:
+    print(f"{number} is an Odd number.")
